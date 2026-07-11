d4f2f088a75c3ba03c6ea5287d208a1ad954c739
drawtheomega <tomas.kamegawa@hotmail.com>
2026-07-11 19:31:37 -0300

    .

---
+++ Scripts/07-4-backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#ROOT
+
+mountpoint -q $LFS/dev/shm && umount $LFS/dev/shm
+umount $LFS/dev/pts
+umount $LFS/{sys,proc,run,dev}
+cd $LFS
+tar -cJpf $HOME/lfs-temp-tools-13.0-systemd.tar.xz .
+
+echo "Luego del backup y para seguir en chroot ejecutar el script 07-01-first-chroot.sh ya que no va a haber ninguna unidad montada"
