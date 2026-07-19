f18974771ed7897ea62cf3405909afb016ef6070
drawtheomega <tomas.kamegawa@hotmail.com>
2026-07-19 17:53:46 -0300

    .

--- Scripts/08-2-pendientes.sh
+++ Scripts/08-2-pendientes.sh
@@ -510,3 +510,32 @@ su tester -c "make -k check"
 make install
 cd /sources
 rm -rf
+
+echo "E2fsprogs-1.47.3"
+sleep 3
+echo "Extrayendo e2fsprogs-1.47.3.tar.gz"
+tar -xf e2fsprogs-1.47.3.tar.gz
+cd e2fsprogs-1.47.3
+mkdir -v build
+cd       build
+../configure --prefix=/usr       \
+             --sysconfdir=/etc   \
+             --enable-elf-shlibs \
+             --disable-libblkid  \
+             --disable-libuuid   \
+             --disable-uuidd     \
+             --disable-fsck
+make
+make check
+make install
+rm -fv /usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
+gunzip -v /usr/share/info/libext2fs.info.gz
+install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info
+makeinfo -o      doc/com_err.info ../lib/et/com_err.texinfo
+install -v -m644 doc/com_err.info /usr/share/info
+install-info --dir-file=/usr/share/info/dir /usr/share/info/com_err.info
+sed 's/metadata_csum_seed,//' -i /etc/mke2fs.conf
+cd /sources
+rm -rf e2fsprogs-1.47.3
+
+echo "Es recomendable hacer un backup ahora, ya que en la limpieza de archivos basura puede romperse algo"
